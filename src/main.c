08c43723a2ed9dc2deedbddfb72b8633b95880a5
khosta77 <puwerfulpants@gmail.com>
2024-01-15 15:01:59 +0300

    Прозвонка I2C

--- src/main.c
+++ src/main.c
@@ -1,11 +1,71 @@
 #include "../system/include/cmsis/stm32f4xx.h"
 
-int main(void) {
-    // Init
+#define BME280_ADDRESS 0b01110111
 
-	while(1) {  // Worker
-
-	}
+void I2C1_init() {
+    // GPIO init
+    // PB6 I2C_SCL 
+    // PB9 I2C_SDA
+    // эти пины выбраны, так как подключены, к 4.7 кОм
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+    GPIOB->MODER |= (GPIO_MODER_MODER6_1 | GPIO_MODER_MODER9_1);
+    GPIOB->OTYPER |= (GPIO_OTYPER_OT_9 | GPIO_OTYPER_OT_6);
+    GPIOB->PUPDR |= (GPIO_PUPDR_PUPDR9_0 | GPIO_PUPDR_PUPDR6_0);
+    GPIOB->AFR[0] |= GPIO_AFRL_AFSEL6_2;
+    GPIOB->AFR[1] |= GPIO_AFRH_AFSEL9_2;
+    // I2C init
+    RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
+    // 1. Устанавливает тактовую частоту периферийного устройства
+    I2C1->CR2 = 0x2A;
+    
+    // 2. Задаем частоту работы, есть два режима стандартная и повышенная, выставлена стандартная
+    I2C1->CCR = 0xD2;
+    
+    // 3. Настройка скорости
+    I2C1->TRISE = 0x2B;
+    
+    // 4. Включаем переферию
+    I2C1->CR1 |= I2C_CR1_PE;
 }
 
+void I2C1_write(uint8_t address) {
+	// 0. Ждем не занят ли шина I2C
+	while (I2C1->SR2 & I2C_SR2_BUSY);
+	
+    // 1. Запускаем передачу
+	I2C1->CR1 |= I2C_CR1_START;
+	
+    // 2. Ждем пока будет отправлен начальный бит
+	while (!(I2C1->SR1 & I2C_SR1_SB));
+	
+    // 3. Отправляем в канал адрес, для того чтобы происходила запись данных \
+    //    его надо сместить в лево на 1 бит и оставить ноль первым битом
+	I2C1->DR = ((address << 1) | 0x01);
 
+	// 4. Ждем пока предет подверждение получения адреса
+    uint16_t count = 0;
+    while (!(I2C1->SR1 & I2C_SR1_ADDR)) {
+        if (count == 1000) break;
+        ++count;
+    }
+
+    if (count != 1000) {
+        GPIOD->ODR |= GPIO_ODR_OD12;
+    }
+
+    (void) I2C1->SR2;
+	I2C1->CR1 |= I2C_CR1_STOP;
+}
+
+void GPIOD_init() {
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
+    GPIOD->MODER |= GPIO_MODER_MODER12_0;
+}
+
+int main(void) {
+    I2C1_init();
+    GPIOD_init();
+    for (uint8_t i = 0; i < 128; i++) {
+        I2C1_write(i);
+    }
+}

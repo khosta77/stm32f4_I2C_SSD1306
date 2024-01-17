8f89d83fbc17da6588218d542a03d071755ba920
khosta77 <puwerfulpants@gmail.com>
2024-01-18 00:03:13 +0300

    Расписал инициализацию I2C, что за настройка частоты и тд

--- src/main.c
+++ src/main.c
@@ -13,19 +13,42 @@ void I2C1_init() {
     GPIOB->PUPDR |= (GPIO_PUPDR_PUPDR9_0 | GPIO_PUPDR_PUPDR6_0);
     GPIOB->AFR[0] |= GPIO_AFRL_AFSEL6_2;
     GPIOB->AFR[1] |= GPIO_AFRH_AFSEL9_2;
+
     // I2C init
     RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
-    // 1. Устанавливает тактовую частоту периферийного устройства
-    I2C1->CR2 = 0x2A;
+    // 1. Устанавливает тактовую частоту периферийного устройства 50 MHz
+    I2C1->CR2 |= (I2C_CR2_FREQ_5 | I2C_CR2_FREQ_4 | I2C_CR2_FREQ_1);  // 0x2A;
     
-    // 2. Задаем частоту работы, есть два режима стандартная и повышенная, выставлена стандартная
-    I2C1->CCR = 0xD2;
+    // 2. Задаем частоту работы
+    // Есть два режима стандартная и повышенная:
+    // - Sm, стандартный, скорость 100 кГц = 10  uS = 10 000 nS
+    // - Fs, повышенный, скорость 400 кГц  = 2.5 uS =  2 500 nS
+    // DUTY скважность
+    // Если SM:
+    // t_h = CCR * t_PCLK1
+    // t_l = CCR * t_PCLK1
+    // Если FM:
+    //           DUTY = 0       |        DUTY = 1
+    // t_h = 01 * CCR * t_PCLK1 | t_h = 09 * CCR * t_PCLK1
+    // t_l = 02 * CCR * t_PCLK1 | t_l = 16 * CCR * t_PCLK1
+    // Из этого складывается t_SCL = t_h + t_l
+    // Пример расчета для 400 kHz, когда FM=1,DUTY=0:
+    // CCR * t_PCLK1 + 2 CCR * t_PCLK1 = 2 500 nS
+    // CCR = 2500 nS / (3 * t_PCLK1) => округлить до целого и перевести в hex.
+    // t_PCLK1 = 1 / f_APB1, к APB1 подключены все I2C
+    I2C1->CCR |= (I2C_CCR_FS | 0x00C);
     
-    // 3. Настройка скорости
-    I2C1->TRISE = 0x2B;
+    // 3. Определим время нарастания фронта, по-моему ни на что не влияет.
+    // Tr = xM
+    // - SM 1000 nS
+    // - FM 300 nS
+    // TRISE = (Tr / t_PCLK1) + 1
+    I2C1->TRISE |= 0x0006;
     
     // 4. Включаем переферию
-    I2C1->CR1 |= I2C_CR1_PE;
+    I2C1->CR1 |= (I2C_CR1_PE);
+
+    // https://hubstub.ru/stm32/184-stm32-i2c.html - помогло
 }
 
 void I2C1_write(uint8_t address) {
